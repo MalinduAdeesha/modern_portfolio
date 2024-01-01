2c6599c1bed41be7138461e2731c8fc25b644247
MalinduAdeesha <malinduadeesha63@gmail.com>
2024-01-01 15:41:03 +0530

    commit

---
+++ .next/static/chunks/pages/services-fe40fce6da22654f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[54],{5325:function(n,e,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/services",function(){return u(5584)}])},5584:function(n,e,u){"use strict";u.r(e);var i=u(5893);let _=()=>(0,i.jsx)("div",{children:"Services"});e.default=_}},function(n){n.O(0,[774,888,179],function(){return n(n.s=5325)}),_N_E=n.O()}]);
